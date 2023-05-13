c183001eaf440c9e0fce568021ee5749357fe256
m292983 <cleitonluiz@sisconsultoria.com.br>
2023-05-12 22:24:39 -0300

    Adicionando rotas de Roles

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import swaggerDocument from '../swagger.json'
 import { TreatmentError } from './middleware/treatment-error';
 import { AccountRouters } from './modules/account/account.routers';
 import { authUser } from './modules/auth/auth.routers';
+import { RoleRouters } from './modules/role/role.routers';
 
 export class App {
   public server: express.Application = express();
@@ -40,6 +41,7 @@ export class App {
 
   private router() {
     this.server.use(authUser)
+    this.server.use('/role', RoleRouters)
     this.server.use('/account', AccountRouters)
     // * Middleware de tratamento de erro
     // ! Deixar sempre como a ultima rota 
